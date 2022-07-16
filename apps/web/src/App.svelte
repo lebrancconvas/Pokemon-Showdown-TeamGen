c61cfd88506ca4e570fbde84c13b815ac2bd9055
lebrancconvas <lebrancconvas@gmail.com>
2022-07-17 04:11:25 +0700

    ✨ Create Search Feature.

--- apps/web/src/App.svelte
+++ apps/web/src/App.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
+  let searchName = "";
 
+  const generated = () => {
+    console.log(searchName); 
+  };
 </script>
 
 <main>
@@ -11,12 +15,19 @@
       <h3>Generated the perfect team for your Pokemon Battle</h3>
     </div>
   </header>
+  <section id="search">
+    <div>
+      <input type="text" placeholder="Search Pokemon Name" bind:value={searchName} required>
+    </div>
+  </section>
   <section id="image">
-
+    <div>
+      <p>Image Section</p>
+    </div>
   </section>
   <section id="generator">
-    <div>
-      <button>Generated</button>
+    <div id="gen-button" on:click={generated}>
+      Generated       
     </div>
   </section>
   <footer>
@@ -27,5 +38,28 @@
 <style>
   main {
     text-align: center;
+    font-family: 'Nunito', sans-serif; 
+  }
+
+  #gen-button {
+    padding: 10px;
+    width: 10%;
+    background-color: #39A3D0FF;
+    margin: 0 auto;
+    cursor: pointer;
+    border-radius: 5px;
+  }
+
+  #gen-button:hover {
+    background-color: #44B7E8FF;
+  }
+
+  #gen-button:active {
+    transform: scale(0.95);
+  }
+
+  input {
+    height: 30px;
+    width: 30%;
   }
 </style>
